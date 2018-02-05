92bf2e301da8738587686aa7d11a380e200929d4
pirumpi <pirumpi@gmail.com>
2018-02-05 14:26:01 -0700

    Adding missing file

---
+++ config.sh
@@ -0,0 +1,3 @@
+export BOOST_VERSION="1.65.1"
+export BOOST_FILENAME="boost_1_65_1.tar.gz"
+export BOOST_DOWNLOAD_URL="http://downloads.sourceforge.net/project/boost/boost/$BOOST_VERSION/$BOOST_FILENAME"
